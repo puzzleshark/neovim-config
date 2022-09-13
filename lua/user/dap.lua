0caa8d63f39ad781b522527fd4e01cd321ce7c96
Samuel Banning <samcbanning@gmail.com>
2022-09-12 23:37:09 -0400

    debug seems to be working

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -1,2 +1,19 @@
--- require('dap-python').setup('/usr/bin/python3')
-require("mason").setup()
+local dap = require("dap")
+
+dap.adapters.python = {
+  type = "executable";
+  command = "/usr/bin/python3";
+  args = { "-m", "debugpy.adapter" };
+}
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch';
+    name = "Launch file";
+    program = "${file}"; -- This configuration will launch the current file if used.
+    pythonPath = '/usr/bin/python3';
+
+  },
+}
